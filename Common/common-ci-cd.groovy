9362069d7f68183f1dd2c104932d349a5e4dc8b9
jayanthikr91 <jayanthikr91@gmail.com>
2019-01-18 12:41:30 +0530

    updated

--- Common/common-ci-cd.groovy
+++ Common/common-ci-cd.groovy
@@ -1,5 +1,27 @@
-def sendMail(prop){
-stage('success email'){
-    emailext body: '${DEFAULT_CONTENT}', subject: '${DEFAULT_SUBJECT}', to: prop.RECEPIENT_MAIL_ID
-  }
+def sendSuccessMail(prop){
+
+   emailext body: '${DEFAULT_CONTENT}', subject: '${DEFAULT_SUBJECT}', to: prop.RECEPIENT_MAIL_ID
+ 
 }
+def sendFailureMail(prop){
+    emailext body: '${DEFAULT_CONTENT}', subject: '${JOB_NAME} - BUILD # ${BUILD_NUMBER} -  FAILURE', to: prop.RECEPIENT_MAIL_ID
+}
+def clearWorkspace(){
+deleteDir()
+}
+
+def uploadArtifact(prop){
+def server = Artifactory.server prop.ARTIFACT_ID
+def uploadSpec = """{
+ 	
+"files":[
+{
+"pattern":"${prop.ARTIFACT_PATTERN}",
+"target":"${prop.ARTIFACT_REPO}/${pom.artifactId}/${pom.version}.${BUILD_NUMBER}/"
+}
+]
+}"""
+server.upload(uploadSpec)
+
+}
+
