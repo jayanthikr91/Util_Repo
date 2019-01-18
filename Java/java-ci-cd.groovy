4d9b6563018fd45e650d089a3c16b3e65c184fe2
jayanthikr91 <jayanthikr91@gmail.com>
2019-01-18 12:41:58 +0530

    updated

--- Java/java-ci-cd.groovy
+++ Java/java-ci-cd.groovy
@@ -1,12 +1,10 @@
 def execute(prop){
 try{
 def pom    
+def commonutility=load prop.COMMON_GROOVY_FILE_LOC    
 stage('checkout')
 
 {
-
-def giturl=prop.GIT_URL
-echo "giturl=${giturl}"
 git prop.GIT_URL
 pom=readMavenPom  file: prop.POM_FILE
 }
@@ -22,31 +20,26 @@ stage('code analysis'){
     echo "${prop.MAVEN_SONAR_CMD}+${prop.SONAR_URL}"
     sh prop.MAVEN_SONAR_CMD +prop.SONAR_URL
 }
+    
 stage('Artifactory upload')
 {
-def server = Artifactory.server prop.ARTIFACT_ID
-def uploadSpec = """{
- 	
-"files":[
-{
-"pattern":"${prop.WAR_PATTERN}",
-"target":"hello/${pom.artifactId}/${pom.version}.${BUILD_NUMBER}/"
+commonutility.uploadArtifact(prop);
+    
 }
-]
-}"""
-server.upload(uploadSpec)
-}
+    
 stage ('Final deploy'){
 echo "${prop.SECURE_COPY_CMD}+${prop.SRC_DEPLOY_LOC}+${prop.DEST_DEPLOY_LOC}"
 sh prop.SECURE_COPY_CMD+prop.SRC_DEPLOY_LOC+" "+prop.DEST_DEPLOY_LOC
 }
+    
 stage('success email'){
-    emailext body: '${DEFAULT_CONTENT}', subject: '${DEFAULT_SUBJECT}', to: prop.RECEPIENT_MAIL_ID
+    commonutility.sendSuccessMail(prop);
+    
   }
-  }
+ }
   catch(Exception e){
   stage('failure email'){
-    emailext body: '${DEFAULT_CONTENT}', subject: '${DEFAULT_SUBJECT}', to: prop.RECEPIENT_MAIL_ID
+    commonutility.sendFailureMail();
   }
   }
 
