3fc36f417e6fa9d2c0a5dd42174173add22c9269
jayanthikr91 <jayanthikr91@gmail.com>
2019-01-18 19:23:15 +0530

    Update java-ci-cd.groovy

--- Java/java-ci-cd.groovy
+++ Java/java-ci-cd.groovy
@@ -21,26 +21,20 @@ stage('code analysis'){
     echo "${prop.MAVEN_SONAR_CMD}+${prop.SONAR_URL}"
     sh prop.MAVEN_SONAR_CMD +prop.SONAR_URL
 }
-    
-stage('Artifactory upload')
-{
-commonutility.uploadArtifact(prop);
-    
-}
-    
+       
 stage ('Final deploy'){
-echo "${prop.SECURE_COPY_CMD}+${prop.SRC_DEPLOY_LOC}+${prop.DEST_DEPLOY_LOC}"
+
 sh prop.SECURE_COPY_CMD+prop.SRC_DEPLOY_LOC+" "+prop.DEST_DEPLOY_LOC
 }
     
 stage('success email'){
-    commonutility.sendSuccessMail(prop);
+    emailext body: '${DEFAULT_CONTENT}', subject: '${DEFAULT_SUBJECT}', to: prop.RECEPIENT_MAIL_ID
     
   }
  }
   catch(Exception e){
   stage('failure email'){
-    commonutility.sendFailureMail();
+    emailext body: '${DEFAULT_CONTENT}', subject: '${JOB_NAME} - BUILD # ${BUILD_NUMBER} -  FAILURE', to: prop.RECEPIENT_MAIL_ID
   }
   }
 
